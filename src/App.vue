b1a0552d8ae6914c11ce8e0eee45791b71c9b37b
PauloParis <paul1963.1969@gmail.com>
2023-04-01 20:02:44 -0300

    basic provide inject

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>App Contador: {{ contador }}</h1>
+    <ContadorUno/>
+    <contadorAccion/>
+  </div>
+</template>
+
+
+<script>
+import { provide, ref } from 'vue'
+import ContadorUno from './components/ContadorUno.vue';
+import ContadorAccion from './components/ContadorAccion.vue';
+export default {
+  components: {
+    ContadorUno,
+    ContadorAccion
+
+  },
+  setup(){
+    const contador = ref(0);
+
+    provide('contador', contador) // puede ser accedido a cualquier componente secundario
+
+    return {
+      contador
+    }
+  }
+}
+</script>
